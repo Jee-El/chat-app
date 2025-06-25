84fcb1e166866f4f5be00d16a6dc145096b902a6
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-06-25 16:25:30 +0100

    docs: clarify why ttl>0 is checked post-middleware

--- backend/src/routes/users/me.ts
+++ backend/src/routes/users/me.ts
@@ -100,6 +100,10 @@ meRoute.delete("/", async (c) => {
     }
 
     const ttl = exp - Math.floor(Date.now() / 1000);
+
+    // This check is just for exteremely rare edge case where
+    // the token expires right after it's validated by jwt
+    // middleware, and before this chunk of code runs
     if (ttl > 0) {
         await redis.set(jti, "blacklisted");
         await redis.expire(jti, ttl);
