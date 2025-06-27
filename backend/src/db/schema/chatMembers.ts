3a8b08599ab33107d30b8385540af1bfcf845dac
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-06-27 20:42:45 +0100

    fix: prevent duplicated chat members

--- backend/src/db/schema/chatMembers.ts
+++ backend/src/db/schema/chatMembers.ts
@@ -1,20 +1,22 @@
-import {
-    pgTable,
-    boolean,
-    integer,
-    serial,
-} from "drizzle-orm/pg-core";
+import { pgTable, boolean, integer, serial } from "drizzle-orm/pg-core";
 
-import { chats } from "./chats"
-import { users } from "./users"
+import { chats } from "./chats";
+import { users } from "./users";
+import { unique } from "drizzle-orm/pg-core";
 
-export const chatMembers = pgTable("chat_members", {
-    id: serial().primaryKey(),
-    userId: integer("user_id")
-        .references(() => users.id, { onDelete: "cascade" })
-        .notNull(),
-    chatId: integer("chat_id")
-        .references(() => chats.id, { onDelete: "cascade" })
-        .notNull(),
-    isAdmin: boolean("is_admin").default(false).notNull(),
-});
+export const chatMembers = pgTable(
+    "chat_members",
+    {
+        id: serial().primaryKey(),
+        userId: integer("user_id")
+            .references(() => users.id, { onDelete: "cascade" })
+            .notNull(),
+        chatId: integer("chat_id")
+            .references(() => chats.id, { onDelete: "cascade" })
+            .notNull(),
+        isAdmin: boolean("is_admin").default(false).notNull(),
+    },
+    (table) => [
+        unique("unique_userid_chatid").on(table.userId, table.chatId),
+    ]
+);
